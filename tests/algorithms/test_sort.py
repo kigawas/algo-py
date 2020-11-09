a02186cf49d2fc6bd23a7f373f69680ccdc0d8ae
Weiliang Li <to.be.impressive@gmail.com>
2020-11-09 16:41:03 +0900

    select k

--- tests/algorithms/test_sort.py
+++ tests/algorithms/test_sort.py
@@ -1,5 +1,7 @@
 from random import shuffle
 
+from algo.algorithms.array.select import partition
+from algo.algorithms.array.select import select_kth_smallest
 from algo.algorithms.array.sort import merge
 from algo.algorithms.array.sort import merge_sort
 from algo.algorithms.array.sort import quick_sort
@@ -11,8 +13,31 @@ def test_qs():
     assert quick_sort(a) == sorted(a)
 
 
+def check(array, pivot_index):
+    p = array[pivot_index]
+    r = partition(array, pivot_index)
+    for i in array[:r]:
+        assert i <= p
+
+    for i in array[r:]:
+        assert i >= p
+
+
 def test_ms():
     merge([1, 3, 4, 5], [0, 2, 6]) == sorted(list(range(0, 7)))
     a = list(range(0, 1000))
     shuffle(a)
     assert merge_sort(a) == sorted(a)
+
+    for i in range(10):
+        b = list(range(10))
+        shuffle(b)
+        check(b, i)
+
+
+def test_select():
+
+    for i in range(10):
+        b = list(range(10))
+        shuffle(b)
+        assert select_kth_smallest(b, i) == i
