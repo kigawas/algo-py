e80fc69fcd44783c3417a79527cd99b4b8a21c1b
Weiliang Li <to.be.impressive@gmail.com>
2020-11-09 18:02:28 +0900

    monotonic

--- algo/applications/sum_of_subarray_minimums.py
+++ algo/applications/sum_of_subarray_minimums.py
@@ -2,20 +2,37 @@ from typing import List
 
 from algo.algorithms.array.stack import monotonic
 
+"""
+Leetcode problem: https://leetcode.com/problems/sum-of-subarray-minimums/
+
+Input: [3,1,2,4]
+Output: 17
+Explanation: Subarrays are [3], [1], [2], [4], [3,1], [1,2], [2,4], [3,1,2], [1,2,4], [3,1,2,4].
+Minimums are 3, 1, 2, 4, 1, 1, 2, 1, 1, 1.  Sum is 17.
+"""
+
 
 def solve(A: List[int]) -> int:
     arr = [0] + A + [0]
-    area = 0
+    res = 0
 
     def should_pop(index: int, mono: List[int]) -> bool:
-        nonlocal area
-
+        nonlocal res
         cond = arr[mono[-1]] > arr[index]
         if cond:
+            # invariant:
+            # arr[mono[i]] <= arr[mono[i+1]], arr[mono[-1]] <= arr[index]
+            # if violated, we need to clear the mono stack and count minimums
             mono_right = mono[-1]
             mono_left = mono[-2]
-            area += arr[mono_right] * (index - mono_right) * (mono_right - mono_left)
+            # ----> minimum value * A * B
+            res += arr[mono_right] * (index - mono_right) * (mono_right - mono_left)
+            # A == count of array containing arr[mono[-1]] as minimum, arr[mono[-1]] is the leftest
+            # e.g. 1 in [3, 1, 2, 4]: [1], [1, 2], [1, 2, 4]
+            # B == count of array not containing arr[mono[-1]] plus 1, arr[mono[-1]] is the neighbor
+            # e.g. 1 in [3, 1, 2, 4]: [3]
+            # multiply 3 * (1 + 1): [1], [1, 2], [1, 2, 4], [3, 1], [3, 1, 2], [3, 1, 2, 4]
         return cond
 
     monotonic(len(arr), should_pop)
-    return area % (10 ** 9 + 7)
+    return res % (10 ** 9 + 7)
--- tests/applications/test_lc_problems.py
+++ tests/applications/test_lc_problems.py
@@ -1,5 +1,10 @@
-from algo.applications import super_egg_drop
+from algo.applications import super_egg_drop, sum_of_subarray_minimums
 
 
 def test_super_egg_drop():
     assert 16 == super_egg_drop.solve_by_dp(4, 2000)
+
+
+def test_sum_of_subarray_minimums():
+    assert 17 == sum_of_subarray_minimums.solve([3, 1, 2, 4])
+    assert 20 == sum_of_subarray_minimums.solve([1, 2, 3, 4])
